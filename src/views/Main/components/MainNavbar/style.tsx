9777e7caecd37ae93bd99d7e493582c53d92d9ab
QiuSi2020 <79633002+QiuSi2020@users.noreply.github.com>
2022-11-08 17:20:08 +0800

    Update style.tsx

--- src/views/Main/components/MainNavbar/style.tsx
+++ src/views/Main/components/MainNavbar/style.tsx
@@ -2,12 +2,16 @@ import styled, {variables} from '@/assets/style'
 
 const unitPx = variables.TabAndContent_px
 
+const themeColor = variables.themeColor
+
 // Navbar卡片样式
 export const Navbar = styled.div.attrs({className: 'NavbarCss'})`
 
     &>.ant-tabs>.ant-tabs-nav {
-        margin: 0 ${unitPx};
-        margin-bottom: ${unitPx};
+        
+        /* margin: ${unitPx}; */
+        margin: 0;
+        padding: ${unitPx};
 
         /* 让 额外操作 贴底 */
         display: flex;
@@ -22,22 +26,39 @@ export const Navbar = styled.div.attrs({className: 'NavbarCss'})`
                     // 一个 Tab+顶部外边距 通常为40px
 
                     width: 120px;
-                    border-radius: 0 10px;
                     display: flex;
                     justify-content: space-between;
 
+                    /* Tab内标题超出隐藏 */
                     &>.ant-tabs-tab-btn {
                         overflow: hidden;
                         text-overflow: ellipsis;
                     }
 
-                    // 每一个Tab距离顶部的高度
-                    /* --MarginTop: 4px;
-                    height: calc(40px - var(--MarginTop, 4px));
-                    margin-top: var(--MarginTop, 5px); */
-                    
-                    margin-top: ${unitPx};
+                    /* Tab删除按钮 */
+                    &>.ant-tabs-tab-remove {
+                        margin: 0;
+                    }
+
+                    /* margin-right: ${unitPx} !important; */
                 }
+
+                .ant-tabs-tab:hover {
+                    min-width: 100%;
+                }
+
+                /* 选中样式 */
+                .ant-tabs-tab-active {
+                    width: 135px !important;
+                    border: 1px solid ${themeColor};
+                    border-radius: 7px;
+                    box-shadow: 0px 2px 7px 0px rgba(85, 110, 97, 0.35);
+                    /* 选中字体颜色 */
+                    &>.ant-tabs-tab-btn {
+                        color: ${themeColor};
+                    }
+                }
+                
             }
         }
     }
